7eb17b4828e484febf5f2ec33a4582415ccfd730
AdarshSNair <66113671+AdarshSNair@users.noreply.github.com>
2021-06-15 19:53:08 +0530

    auth and redux

---
+++ firebase.js
@@ -0,0 +1,10 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyA70mdcTyEPwBbdIrSGFxZD5lm07rZW_kE",
+    authDomain: "e-clone-3372c.firebaseapp.com",
+    projectId: "e-clone-3372c",
+    storageBucket: "e-clone-3372c.appspot.com",
+    messagingSenderId: "910897670398",
+    appId: "1:910897670398:web:916e347ac6c25c8fad1daf"
+  };
--- src/components/Product.js
+++ src/components/Product.js
@@ -1,15 +1,37 @@
 import Image from "next/image";
 import { useState } from "react";
 import {StarIcon} from "@heroicons/react/solid"
+import { useDispatch } from "react-redux";
+import {addToBasket} from "../slices/basketSlice"
 
 const MAX_RATING=5;
 const MIN_RATING=1;
 
 function Product({id,title,price,description,category,image}) {
+
+    const dispatch =useDispatch();
+
     const [rating] =useState(
         Math.floor(Math.random() * (MAX_RATING -MIN_RATING + 1)) + MIN_RATING
     );
     const [hasPrime] = useState(Math.random() < 0.5);
+
+    const addItemToBasket =() => {
+        const product ={
+            id,
+            title,
+            price,
+            description,
+            category,
+            image,
+            hasPrime,
+            rating
+        };
+
+      
+        dispatch(addToBasket(product))
+    };
+
 
 
     return (
@@ -45,7 +67,7 @@ function Product({id,title,price,description,category,image}) {
                 </div>
             )}
 
-            <button className="mt-auto button">Add to Basket</button>
+            <button onClick={addItemToBasket} className="mt-auto button">Add to Basket</button>
 
         </div>
     )
